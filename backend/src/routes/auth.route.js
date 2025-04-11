7e2aa829d7291417f5242007b802d20c2cde49b4
Zhou Yuhang <zyuhang255@gmail.com>
2025-04-11 17:48:10 +0800

    Create boiler template routes and models

---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import { authCallback } from "../controller/auth.controller.js";
+
+const router = Router();
+
+router.post("/callback", authCallback);
+export default router;
