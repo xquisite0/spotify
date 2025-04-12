eeaee6f2ea53bd48a725b7b9cc3378ee62b7b02c
Zhou Yuhang <zyuhang255@gmail.com>
2025-04-12 11:08:24 +0800

    Initialise main layout with left & right sidebars

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage";
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
+import MainLayout from "./layout/MainLayout";
+import ChatPage from "./pages/chat/ChatPage";
 
 function App() {
   // token =>
@@ -9,7 +11,6 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
         <Route
           path="/sso-callback"
           element={
@@ -19,6 +20,11 @@ function App() {
           }
         />
         <Route path="/auth-callback" element={<AuthCallbackPage />} />
+
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/chat" element={<ChatPage />} />
+        </Route>
       </Routes>
     </>
   );
---
+++ frontend/src/layout/MainLayout.tsx
@@ -0,0 +1,49 @@
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+const MainLayout = () => {
+  const isMobile = false;
+  return (
+    <div className="h-screen bg-black text-white flex flex-col">
+      <ResizablePanelGroup
+        direction="horizontal"
+        className="flex-1 flex h-full overflow-hidden p-2"
+      >
+        {/* Left sidebar */}
+        <ResizablePanel
+          defaultSize={20}
+          minSize={isMobile ? 0 : 10}
+          maxSize={30}
+        >
+          left sidebar
+        </ResizablePanel>
+
+        <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
+
+        {/* Main content */}
+        <ResizablePanel defaultSize={isMobile ? 80 : 60}>
+          <Outlet />
+        </ResizablePanel>
+
+        <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
+
+        {/* Right sidebar */}
+        <ResizablePanel
+          defaultSize={20}
+          minSize={0}
+          maxSize={25}
+          collapsedSize={0}
+        >
+          friends activity
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </div>
+  );
+};
+
+export default MainLayout;
